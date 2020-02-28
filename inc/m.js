c467694d14915975fa47db526b786708c26907ce
nohyoungjin <45346737+nohyoungjin@users.noreply.github.com>
2020-02-28 11:18:34 +0900

    include 수정

---
+++ inc/m.js
@@ -0,0 +1,9 @@
+str = ''
+str += '<div id="header" class="fix">'
+str += '	<a href="https://nohyoungjin.github.io/pet/m.html">'
+str += '		<i class="ri-arrow-left-s-line"></i>'
+str += '		<span>뒤로가기</span>'
+str += '	</a>'
+str += '</div>'
+
+document.write( str );
