0c5e920e0a86f7182c5e44c0840d9978c19b148c
nohyoungjin <45346737+nohyoungjin@users.noreply.github.com>
2020-03-30 15:25:49 +0900

    update

---
+++ inc/slide.js
@@ -0,0 +1,13 @@
+str = ''
+str += '<div class="swiper-view swiper-container">'
+str += '	<div id="swiper-wrap" class="swiper-wrapper"></div>'
+str += '	<div class="swiper-pagination"></div>'
+str += '	<button class="slideshow_nav slide_nav slideshow_prev" role="button" aria-label="Previous slide">'
+str += '		<i class="prev"></i>'
+str += '	</button>'
+str += '	<button class="slideshow_nav slide_nav slideshow_next" role="button" aria-label="Next slide">'
+str += '		<i class="next"></i>'
+str += '	</button>'
+str += '</div>'
+
+document.write( str );
