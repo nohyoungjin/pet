e86733fee5fff4878f844bd5622f22ce6acb82c3
nohyoungjin <45346737+nohyoungjin@users.noreply.github.com>
2020-05-20 17:57:44 +0900

    up

--- inc/header_view.js
+++ inc/header_view.js
@@ -22,7 +22,7 @@ if ( oParams.link == 'home' ) {
 
 	str = ''
 	str += '<div id="header" class="fix">'
-	str += '	<a href="./././view.html#id=' + oParams.id + '">'
+	str += '	<a href="./././view.html#' + oParams.id + '">'
 	str += '		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M7.828 11H20v2H7.828l5.364 5.364-1.414 1.414L4 12l7.778-7.778 1.414 1.414z" fill="rgba(231,76,60,1)"/></svg>'
 	str += '		<span>뒤로가기</span>'
 	str += '	</a>'
