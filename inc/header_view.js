75764eefb2cb27363241eb64aac6472af379368e
nohyoungjin <45346737+nohyoungjin@users.noreply.github.com>
2020-05-20 17:54:24 +0900

    up

---
+++ inc/header_view.js
@@ -0,0 +1,33 @@
+//
+
+function getUrlParams() {
+    var params = {};
+    window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(str, key, value) { params[key] = value; });
+    return params;
+}
+
+oParams = getUrlParams();
+
+if ( oParams.link == 'home' ) {
+
+	str = ''
+	str += '<div id="header" class="fix">'
+	str += '	<a href="javascript:history.go(-1);">'
+	str += '		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M7.828 11H20v2H7.828l5.364 5.364-1.414 1.414L4 12l7.778-7.778 1.414 1.414z" fill="rgba(231,76,60,1)"/></svg>'
+	str += '		<span>뒤로가기</span>'
+	str += '	</a>'
+	str += '</div>'
+
+} else {
+
+	str = ''
+	str += '<div id="header" class="fix">'
+	str += '	<a href="./././view.html#id=' + oParams.id + '">'
+	str += '		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M7.828 11H20v2H7.828l5.364 5.364-1.414 1.414L4 12l7.778-7.778 1.414 1.414z" fill="rgba(231,76,60,1)"/></svg>'
+	str += '		<span>뒤로가기</span>'
+	str += '	</a>'
+	str += '</div>'
+
+}
+
+document.write( str );
