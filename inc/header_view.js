0cf765ad23bd8198f4a77f2f649f92fee36d4efc
nohyoungjin <45346737+nohyoungjin@users.noreply.github.com>
2020-05-21 11:19:36 +0900

    update

--- inc/header_view.js
+++ inc/header_view.js
@@ -1,6 +1,6 @@
 //
 
-function getUrlParams() {
+/* function getUrlParams() {
     var params = {};
     window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(str, key, value) { params[key] = value; });
     return params;
@@ -29,5 +29,16 @@ if ( oParams.link == 'home' ) {
 	str += '</div>'
 
 }
+
+document.write( str ); */
+
+
+str = ''
+str += '<div id="header" class="fix">'
+str += '	<a href="javascript:history.go(-1);">'
+str += '		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M7.828 11H20v2H7.828l5.364 5.364-1.414 1.414L4 12l7.778-7.778 1.414 1.414z" fill="rgba(231,76,60,1)"/></svg>'
+str += '		<span>뒤로가기</span>'
+str += '	</a>'
+str += '</div>'
 
 document.write( str );
