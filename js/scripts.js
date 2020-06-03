ed65c1ffa654314add8aca3ee9f68886eef9c821
nohyoungjin <45346737+nohyoungjin@users.noreply.github.com>
2020-06-03 10:46:57 +0900

    up

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,177 @@
+$(function() {
+
+	// INIT
+	
+	local();
+
+	// area search
+
+	function local() {
+
+		var $body = $('body'),
+			$close_btn = $('#popup_close'),
+			$popup = $('#popup'),
+			_open = true;
+
+		// open
+
+		$(document).on('click', '.local a', function(e) {
+
+			e.preventDefault();
+
+			if ( _open ) {
+
+				$body.addClass('_open');
+				_open = false;
+
+				TweenLite.fromTo($popup, .5, {y: '-100%'}, {y: '0%', ease: Power4.easeOut});
+				TweenLite.fromTo($close_btn, .2, {autoAlpha: 0}, {autoAlpha: 1, ease: Power0.easeNone,delay: .1});
+
+				//
+
+				$('.filters a').on('click', function() {
+
+					var toFilter = $(this).data('filters');
+					
+					if ( toFilter == '전체' ) {
+						
+						$('.local a span').text(toFilter);
+						$('.lst_filter li').removeClass('sort').removeClass('on').removeClass('off');
+						
+						close_action();
+						
+						return;
+					
+					} else {
+
+						$('.local a span').text(toFilter);
+						$('.lst_filter li').removeClass('on');
+						
+					}
+
+					$('.lst_filter li').each(function(i, elem) {
+
+						var filters = $(this).data('filters');
+						$('.lst_filter li').addClass('sort').addClass('off');
+
+						if ( filters == toFilter ) {
+							$(this).addClass('on');
+						}
+
+					});
+					
+					close_action();
+
+				});
+
+			} else {
+
+				close_action();
+
+			}
+
+		});
+
+	   // close
+
+		$close_btn.on('click', function(e) {
+
+			e.preventDefault();
+			close_action();
+
+		});
+
+		// search close action
+
+		function close_action() {
+
+			TweenLite.to($popup, .4, {
+				y: '-100%',
+				ease: Power4.easeOut,
+				onComplete: function() {
+					$body.removeClass('_open');
+
+					_open = true;
+				}
+			});
+
+		}
+
+	}
+
+});
+
+
 // 데이터 생성
 
-var data = [];
+function list_view(data, folder, insert) {
 
-for (var i = 0; i < 100; i++) {
-
-	data.push('아이템' + i);
-
-}
-
-// 데이터로 항목 만들기
-
-for (let i = 0; i < data.length; i++) {
-
-	changeQueue.enqueue({
+	// 위부터 4개 Output Hold
 	
-		execute: function() {
-			const elem = createItem(data[i]);
-			document.getElementById('list').appendChild(elem);
-		}
-	
+	if ( insert == 'latest' ) {
+		
+		data.splice( -8, 4 );
+
+	}
+
+	var insert = document.getElementById(insert),
+		output = '',
+		path = $('body').hasClass('home') ? '' : '../../',
+		uniq = {
+
+			'info'     : 'm',
+			'beauty'   : 'm_1',
+			'cafe'     : 'm_2',
+			'hospital' : 'm_3',
+			'hotel'    : 'm_4',
+			'funeral'  : 'm_5'
+
+		};
+
+	// 데이터로 항목 만들기
+
+	data.forEach(function(item, index) {
+
+		changeQueue.enqueue({
+		
+			execute: function() {
+
+				if (item.area) {
+					output += '	<li id=' + item.num + ' data-filters=' + item.area + '>';
+				} else {
+					output += '	<li id=' + item.num + ' data-filters=' + item.ani + '>';
+				}
+
+				output += '		<a href="' + path + 'html/' + folder + '/' + item.num + '.html?id=' + item.num + '">';
+				output += '			<div class="thumb">';
+				output += '				<span style="background-image:url(' + path + 'img/thumb/' + folder + '/' + uniq[folder] + '_' + item.num + '.jpg)"></span>';
+				output += '			</div>';
+				output += '			<div class="cont">';
+				output += '				<p class="tit">' + item.tit + '</p>';
+
+				if (item.add) {
+					output += '			<p class="txt">' + item.area + ' ' + item.add + '</p>';
+				}
+
+				output += '			</div>';
+				output += '		</a>';
+				output += '	</li>';
+
+				insert.innerHTML = output;
+
+			}
+		
+		});
+
 	});
 
 }
 
-// 반복적으로 큐를 체크하여 30개씩 실행
+// 반복적으로 큐를 체크하여 ?개씩 실행
 
 setInterval(function() {
 
-	for (var i = 0; i < 30 && !changeQueue.isEmpty(); i++) {
+	for (var i = 0; i < 1 && !changeQueue.isEmpty(); i++) {
 
 		var c = changeQueue.dequeue();
 
@@ -39,17 +183,46 @@ setInterval(function() {
 
 	}
 
-}, 0);
+}, 100);
 
-//
 
-function createItem(d) {
 
-	var elem = document.createElement('li');
+// random output
 
-	elem.textContent = d;
-	elem.classList.add('item');
+$.fn.randomize = function(selector) {
 
-	return elem;
+    var $elems = selector ? $(this).find(selector) : $(this).children(),
+        $parents = $elems.parent();
+ 
+    $parents.each(function() {
+        $(this).children(selector).sort(function() {
+            return Math.round(Math.random()) - 0.5;
+        }).detach().appendTo(this);
+    });
+ 
+    return this;
 
-}
+}; 
+
+
+// get parameter
+
+var getParameter = function(param) { 
+	
+	var returnValue, 
+		url = location.href,
+		parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('&'); 
+	
+	for (var i = 0; i < parameters.length; i++) { 
+		
+		var varName = parameters[i].split('=')[0]; 
+		
+		if (varName.toUpperCase() == param.toUpperCase()) { 
+			
+			returnValue = parameters[i].split('=')[1]; 
+			return decodeURIComponent(returnValue); 
+		
+		} 
+	} 
+
+};
